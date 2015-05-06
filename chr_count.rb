5224a30074e6356e29ee86ed733ec7f6d232d7f9
Sana <szareey@gmail.com>
2015-05-06 17:00:32 -0400

    Add regex command

--- chr_count.rb
+++ chr_count.rb
@@ -1,8 +1,13 @@
 def count_letters(str)
   split_count = Hash.new(0)
-  str.split('').each {|letter| split_count[letter] += 1 }
+  str.gsub(/\s*/,'').each_char {|letter| split_count[letter] += 1}
+  puts str
   return split_count
 end
+
+def record_letter_index(str)
+end
+
 
 puts ('Enter any sentence and I will tell you how many of each character there is!')
 my_string = gets.chomp
